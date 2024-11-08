a0c126dd1f088955ffa7cdb7be1fc91baf0d1732
Jibin George <jibingeorge1964@gmail.com>
2024-11-08 07:56:51 +0530

    reverting to same as wendoj

--- next.config.js
+++ next.config.js
@@ -19,10 +19,16 @@ const withPWA = WithPWA({
 // @ts-ignore
 const config = withPWA({
   reactStrictMode: true,
-  output: 'export',
-  images: {
-    unoptimized: true,
-  }
+
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
 });
 
 export default config;
